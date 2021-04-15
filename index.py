0e99705526494044c3a558e044bb4a5651bf0850
ChristianSalas1 <>
2021-04-15 17:22:34 +0000

    trabajo de sumas,restas,division y multiplicasion

---
+++ index.py
@@ -0,0 +1,13 @@
+print("Hola christian")
+a=45
+b=30
+print("suma:",a+b)
+print("resta",a-b)
+print("division",a/b)
+print("multiplicacion",a*b)
+print("suma:",b+a)
+print("resta",b-a)
+print("division",b/a)
+print("multiplicacion",b*a)
+print("¿ella me ama?")
+print("no")
