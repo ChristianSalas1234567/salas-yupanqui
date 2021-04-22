979649c8be5340333f1ac5f83426b41cb2e90077
ChristianSalas1 <>
2021-04-22 13:32:24 +0000

    buen dia hey

--- Areatriangulo.py
+++
@@ -1,9 +0,0 @@
-#variables de entrada
-print("area del triangulo")
-#datos de entrada
-B=int(input("ingrese base:"))
-H=int(input("ingrese haltura:"))
-#proceso
-area=(B*H)/2
-#datos de salida
-print("el area es: ", area)
